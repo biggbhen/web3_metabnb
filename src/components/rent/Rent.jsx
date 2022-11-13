be04c699aeaab9fdf8443fc76d6c2d740793943e
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-11-13 16:20:15 +0100

    styled rent a place

--- src/components/rent/Rent.jsx
+++ src/components/rent/Rent.jsx
@@ -3,8 +3,11 @@ import { Container } from './rentStyles';
 const Rent = () => {
 	return (
 		<Container>
-			<div>
-				<h2>Rent a Place away from Home in the Metaverse</h2>
+			<div className='rent_a_place'>
+				<h2>
+					Rent a <span>Place</span> away from <span>Home</span> in the{' '}
+					<span>Metaverse</span>
+				</h2>
 				<p>
 					we provide you access to luxury and affordable houses in the
 					metaverse, get a chance to turn your imagination to reality at your
--- src/components/rent/rentStyles.js
+++ src/components/rent/rentStyles.js
@@ -4,6 +4,44 @@ export const Container = styled.section`
 	border: 1px solid red;
 	padding: 3% 7%;
 	> * {
-		width: 50%;
+		width: 40%;
+	}
+	.rent_a_place {
+		width: 60%;
+		h2 {
+			font-size: clamp(2.5rem, 5vw, 3.5rem);
+			margin-bottom: 3rem;
+			color: rgba(67, 67, 67, 1);
+			span {
+				color: rgba(160, 34, 121, 1);
+			}
+		}
+		p {
+			font-size: clamp(1.2rem, 2vw, 1.5rem);
+			margin-bottom: 2rem;
+		}
+
+		form {
+			display: flex;
+			input {
+				width: 65%;
+				border: 1px solid black;
+				border-right: none;
+				outline: none;
+				padding: 20px 0;
+				font-size: clamp(0.9rem, 1.5vw, 1.1rem);
+				border-top-left-radius: 10px;
+				border-bottom-left-radius: 10px;
+			}
+			button {
+				width: 35%;
+				border: none;
+				background: linear-gradient(90deg, #a02279 11.45%, #a02279 11.45%);
+				font-size: clamp(0.9rem, 1.5vw, 1.1rem);
+				color: #fff;
+				border-top-right-radius: 10px;
+				border-bottom-right-radius: 10px;
+			}
+		}
 	}
 `;
